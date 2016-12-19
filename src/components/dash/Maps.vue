5cbb9dd478d6b09e430c62fc98becc5c1aea3789
lberger1993 <berger.luciaeve@gmail.com>
2016-12-19 13:33:45 -0500

    Adds files

---
+++ src/components/dash/Maps.vue
@@ -0,0 +1,32 @@
+<script>
+const VueGoogleMap = require('vue-google-maps')
+VueGoogleMap.load({key: 'AIzaSyDbvxvRkkpfLq5DHdIuCCDlZecfwUu83Ig'})
+
+module.exports = {
+  name: 'Maps',
+  data () {
+    return {
+      center: {lat: 10.0, lng: 10.0},
+      markers: [{
+        position: {lat: 10.0, lng: 10.0}
+      }, {
+        position: {lat: 11.0, lng: 11.0}
+      }]
+    }
+  }
+}
+</script>
+<template>
+  <map
+    :center="center"
+    :zoom="7"
+  >
+    <marker 
+      v-for="m in markers"
+      :position.sync="m.position"
+      :clickable="true"
+      :draggable="true"
+      @g-click="center=m.position"
+    ></marker>
+  </map>
+</template>
